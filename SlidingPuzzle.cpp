48b90d463940486dbcaaea1ae75e3d204125f91f
abdurk5507 <111095888+abdurk5507@users.noreply.github.com>
2023-11-11 23:59:55 -0600

    Update SlidingPuzzle.cpp

--- SlidingPuzzle.cpp
+++ SlidingPuzzle.cpp
@@ -1,6 +1,6 @@
 /*********************************************************
 * Summary: This file includes the implementation of "SlidingPuzzle.hpp"
-* Author: Abdur Khan
+* Author: abdurk5507
 * Created: December 4th, 2021
 ********************************************************/
 
